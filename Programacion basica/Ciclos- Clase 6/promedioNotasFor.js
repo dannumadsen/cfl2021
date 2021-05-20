6e3ef3805c9a15e37bab1b7f72dc23024fdc37a5
dannumadsen <danni_mdn@hotmail.com>
2021-05-20 19:29:17 -0300

    ejercicios

--- Programacion basica/Ciclos- Clase 6/promedioNotasFor.js
+++ Programacion basica/Ciclos- Clase 6/promedioNotasFor.js
@@ -2,7 +2,7 @@ let readlineSync=require("readline-sync");
 let nota, suma, promedio, contador
 suma=0
 
-for (contador=1;contador>11;contador++){ //El contador va desde 1 inclusive hasta 10 inclusive//
+for (contador=1;contador<11;contador++){ //El contador va desde 1 inclusive hasta 10 inclusive//
     nota=readlineSync.questionFloat("Ingrese la nota", contador +":");
     suma+=nota;
 }
