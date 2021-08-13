f6e3fe86698a2ee8f98d8550e358c27c0cd680c0
dannumadsen <82971152+dannumadsen@users.noreply.github.com>
2021-08-13 20:23:04 -0300

    clase 18

---
+++ Front End/Clase 18/contador elementos/main.js
@@ -0,0 +1,12 @@
+"use strict";
+let contador = 0;
+function clickear(){
+    contador ++;
+    console.log('hiciste '+ contador+ ' clicks');
+    mostrarClicks();
+}
+
+function mostrarClicks(){
+    let nodoCLick = document.getElementById('txtClick');
+    nodoCLick.innerHTML = 'hiciste ' + contador + ' clicks';
+}
