b25434a492e4d9ac2c01b7e659f7d65022784170
dannumadsen <82971152+dannumadsen@users.noreply.github.com>
2021-09-04 16:48:48 -0300

    ejercicios clase 23

---
+++ POO/clase 23/typeScript/ejemplo1.ts
@@ -0,0 +1,15 @@
+"use strict";
+//Asi se importan las cosas
+import * as readline from "readline-sync";
+
+
+function sumar(numeros: number[]): number {
+    let sum:number=0;
+    for (let i:number = 0; i< numeros.length; i++ ){
+        sum += numeros[i];
+    }
+    return sum;
+}
+
+let arregloNumeros: number[] = [1,2,4,1]
+console.log(sumar(arregloNumeros));
